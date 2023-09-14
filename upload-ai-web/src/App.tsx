e089ab861a5e00adb735a2fa5e1a3d882497626c
Guilherme Cardoso <guilhermecardosowork@gmail.com>
2023-09-13 21:12:34 -0300

    :tada: Aula 3

--- upload-ai-web/src/App.tsx
+++ upload-ai-web/src/App.tsx
@@ -1,8 +1,11 @@
-import { FileVideo, Github, Upload, Wand2 } from "lucide-react";
+import { Github, Wand2 } from "lucide-react";
 import { Button } from "./components/ui/button";
 import { Separator } from "./components/ui/separator";
 import { Textarea } from "./components/ui/textarea";
 import { Label } from "./components/ui/label";
+import { Slider } from "./components/ui/slider";
+import { VideoForm } from "./components/video-form";
+
 import {
   Select,
   SelectContent,
@@ -10,7 +13,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "./components/ui/select";
-import { Slider } from "./components/ui/slider";
+
 
 export function App() {
   return (
@@ -52,41 +55,7 @@ export function App() {
           </p>
         </div>
         <aside className="w-80 space-y-2">
-          <form className="space-y-6">
-            <label
-              htmlFor="video"
-              className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
-            >
-              <FileVideo className="w-4 h-4" />
-              Selecione um vídeo
-            </label>
-
-            <input
-              type="file"
-              id="video"
-              accept="video/mp4"
-              className="sr-only"
-            />
-
-            <Separator />
-
-            <div className="space-y-2">
-              <Label htmlFor="transcription_prompt">
-                Prompt de transcrição
-              </Label>
-              <Textarea
-                id="transcription_prompt"
-                className="h-20 leading-relaxed resize-none"
-                placeholder="Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)"
-              />
-            </div>
-
-            <Button type="submit" className="w-full">
-              Carregar video
-              <Upload className="w-4 h-4 ml-2" />
-            </Button>
-          </form>
-
+          <VideoForm />
           <Separator />
 
           <form className="space-y-6">
